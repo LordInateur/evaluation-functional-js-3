49711dc2a36a66687fdbe6e0e8c1925ba7adc450
Matthieu Fournier <mat.fournier85@gmail.com>
2018-03-02 10:02:58 +0100

    Exercice 1

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,7 +1,40 @@
 const moment = require("moment");
 
 const sortProduct = input => {
-  // Your future job begins here ...
+  // sort
+  input.sort((a,b)=> a.dateAdded > b.dateAdded)
+
+  let ret =  input
+  // groupe by id
+  .reduce((acc, value)=>{
+    if(moment(value.dateAdded).year() > 2000){
+      if(acc[value.name] == undefined){ acc[value.name] = []}
+      acc[value.name].push(value) 
+
+      // format
+      acc[value.name] = acc[value.name]
+      .reduce((acc, value)=> {
+        if(value.monthOfYear == undefined){
+          return [...acc, 
+              {
+                name : value.name, 
+                year : moment(value.dateAdded).year(),
+                monthOfYear : moment(value.dateAdded).month() +1,
+                quantity : value.quantity
+              }]
+        }
+        else{
+          return [...acc, value]
+        }
+      },[])
+    }
+
+    return acc
+  },{})
+
+  //console.log(ret)
+  return ret;
+
 };
 
 module.exports = {
